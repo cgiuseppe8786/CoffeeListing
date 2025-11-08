b4d495b3bfcd6958ccb5def0102ecf48169532ae
Giuseppe Cammarata <c.giuseppe8786@gmail.com>
2025-11-08 13:22:31 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { CoffeeList } from './components/CoffeeList/coffeeList'
 import './global.css'
+import { CoffeeList } from './components/CoffeeList/coffeeList'
 
 export default function App() {
     return (
-        <div className="site">
-            <div className="hero" />
+        <div
+            className="site"
+        >
+            <div
+                className="hero"
+            />
             <main className="panel">
                 <CoffeeList />
             </main>
